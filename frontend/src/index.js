604e344f3b7420c54d4d897ef477d2350e37f08c
Gustavo <gustavo.ovejero@educacion.local>
2025-10-12 04:14:15 -0300

    Fix default route to show shop

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Tienda from './Tienda';
+import App from './App';        // Panel Admin
+import Tienda from './Tienda';  // Tienda Pública
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Tienda />} />
-        <Route path="/admin" element={<App />} />
+        <Route path="/" element={<Tienda />} />           {/* Página principal = Tienda */}
+        <Route path="/admin" element={<App />} />          {/* /admin = Panel Admin */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
